4ea5007e888665af64eb2068dbcfcd94ac0b5f41
trainandhire <projects.trainandhire@gmail.com>
2022-11-01 17:17:15 +0530

    Login and dashboard created and confitured the paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
+
   ],
   imports: [
     BrowserModule,
